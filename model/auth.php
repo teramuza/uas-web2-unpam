d393da2c54386c57886081bfb440971d1f0dfe56
Teuku Raja <teramuza@gmail.com>
2021-07-17 15:55:41 +0700

    remove duplicated method

--- model/auth.php
+++ model/auth.php
@@ -18,27 +18,4 @@ class Auth extends MySqlDb{
         }
         return $obj;
     }
-
-    static function getDataVaksin($id) {
-        return DataVaksin::getData('data_vaksin', "id='".$id."'");
-    }
-
-    static function createDataVaksin($data) {
-        return DataVaksin::createData(
-            'data_vaksin', 
-            "district, faskes_type, faskes_name, identity_number, name, gender, age, birthday, phone_number, addr",
-            "
-                '$data->district', 
-                '$data->faskes_type', 
-                '$data->faskes_name', 
-                '$data->identity_number', 
-                '$data->name', 
-                '$data->gender', 
-                '$data->age', 
-                '$data->birthday', 
-                '$data->phone_number', 
-                '$data->addr'
-            "
-        );
-    }
 }
