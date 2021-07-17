aea8b01a075fbeb187735bee8b155efbda5004fe
LAPTOP-APNV9H5B\wini5 <wini59maylani@gmail.com>
2021-07-17 16:17:50 +0700

    Tambah tanggal dan waktu

--- index.php
+++ index.php
@@ -31,18 +31,24 @@ function generateGender($g) {
    			display: none;
    		}
     </style>
+    <link href="assets/js/dataTables/dataTables.bootstrap.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
 </head>
-<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
 <body>
 <noscript>You need to enable JavaScript to run this app.</noscript>
 	<div class="container col-md-10 mt-2">
-		<center><h3>Data Vaksinasi Covid-19</h3></center>
+		<?php 
+		date_default_timezone_set('Asia/Jakarta');
+		echo '<center><h3>Daftar Peserta Vaksinasi Covid19</h3>
+			<h3>Per '.date('d F Y H:i:s').'</h3>
+		</center>'
+		?>
 		<div class="card">
 			<div class="card-header bg-success text-white ">
 				Data Peserta <a href="tambah.php" class="noPrint btn btn-sm btn-primary float-right">Tambah</a>
 			</div>
 			<div class="card-body">
-				<table class="table table-bordered">
+				<table class="table table-striped table-bordered table-hover" id="dataTables-example">
 					<thead>
 						<tr>
 							<th>No</th>
@@ -68,15 +74,17 @@ function generateGender($g) {
 							<th class="noPrint">
 								<a href="edit.php" class="noPrint btn-warning btn">Ubah</a>
 								<a href="hapus.php" class="noPrint btn-danger btn">Hapus</a>
-								<button type="button" class="noPrint btn btn-primary" value="Cetak Nota" onclick="window.print()">Cetak</button><br>
+								<button type="button" class="noPrint btn btn-primary" onclick="window.print()">Cetak</button>
 							</th>
 						</tr>
 						<?php endforeach; ?>
 					</tbody>
 				</table>
+				<button type="button" class="noPrint btn btn-primary">Logout</button>
 			</div>
 		</div>
 	</div>
+<script src="assets/js/dataTables/jquery.dataTables.js"></script>
 <script type="text/javascript" src="assets/js/jquery-3.6.0.min.js"></script>
 <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
 </body>
